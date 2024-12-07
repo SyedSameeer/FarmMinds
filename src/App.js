6e506af36721da06f2f82c7aa79b1d58d952631a
Venkat11 <vs1789966@gmail.com>
2024-12-08 04:36:34 +0530

    Fix deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import { DarkModeProvider } from './components/DarkModeContext';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
